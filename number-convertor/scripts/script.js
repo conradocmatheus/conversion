64761707385c9a8c45a5d549968481d8335d8770
coradoo <conrado.matheus@protonmail.com>
2023-10-02 10:18:48 -0300

    missing format

--- number-convertor/scripts/script.js
+++ number-convertor/scripts/script.js
@@ -3,6 +3,23 @@ document.getElementById("inputSubmit").onclick = function () {
     let temp
 
     if (document.getElementById("cBtn").selected) {
-
+        temp = document.getElementById("inputField").value
+        temp = Number(temp)
+        temp = toCelsius(temp)
+        document.getElementById("resultLabel").innerHTML = temp + "C"
     }
+    if (document.getElementById("fBtn").selected) {
+        temp = document.getElementById("inputField").value
+        temp = Number(temp)
+        temp = toFarenheit(temp)
+        document.getElementById("resultLabel").innerHTML = temp + "F"
+    }
+}
+
+
+function toFarenheit(temp) {
+    return (temp * (9 / 5) + 32)
+}
+function toCelsius(temp) {
+    return (temp - 32) * (5 / 9)
 }
